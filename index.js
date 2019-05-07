83b9683644100fc20b5e283c3770c45ae327f9f1
ayunas <ayunas@gmail.com>
2019-05-06 23:37:56 -0500

    set up server and set up a get request route to allow fetching of all users in database

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // implement your API here
 const express = require('express');
-
 const server = express();
+const db = require('./data/db');
+
+
+
+server.get('/', (req,res) => {
+    res.send('server is alive');
+})
+
+server.get('/api/users', (req,res) => {
+    db.find()
+    .then( users => {
+        res.json(users);
+    })
+    .catch( err => {
+        console.log('there has been an error');
+        res.json({error : err, message : 'an error occured'});
+    });
+});
 
 server.listen(5000, () => {
     console.log('listening on port 5000');
 })
+
